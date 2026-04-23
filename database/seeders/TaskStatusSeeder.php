8f4ea8740b769b42a644ffae3ec80fd5b9c30f8d
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2026-04-23 11:26:45 +0300

    feat(step3): добавить модель, миграцию и сидер статусов задач

---
+++ database/seeders/TaskStatusSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\TaskStatus;
+use Illuminate\Database\Seeder;
+
+class TaskStatusSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $statuses = [
+            'Новый',
+            'В работе',
+            'На тестировании',
+            'Завершен',
+        ];
+
+        foreach ($statuses as $name) {
+            TaskStatus::create(['name' => $name]);
+        }
+    }
+}
