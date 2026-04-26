1b1b1a06025fdcbaf6b20049bb100c9a6acceec0
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2026-04-26 16:30:10 +0300

    refactor: рефакторинг web.php

--- routes/web.php
+++ routes/web.php
@@ -20,34 +20,30 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Статусы
 Route::resource('task_statuses', TaskStatusController::class)
     ->only(['index'])
     ->names('task_statuses');
 
-Route::resource('task_statuses', TaskStatusController::class)
-    ->except(['index', 'show'])
-    ->middleware('auth')
-    ->names('task_statuses');
-
-// Задачи
 Route::resource('tasks', TaskController::class)
     ->only(['index'])
     ->names('tasks');
 
-Route::resource('tasks', TaskController::class)
-    ->except(['index'])
-    ->middleware('auth')
-    ->names('tasks');
-
-// Метки
 Route::resource('labels', LabelController::class)
     ->only(['index'])
     ->names('labels');
 
-Route::resource('labels', LabelController::class)
-    ->except(['index', 'show'])
-    ->middleware('auth')
-    ->names('labels');
+Route::middleware('auth')->group(function (): void {
+    Route::resource('task_statuses', TaskStatusController::class)
+        ->except(['index', 'show'])
+        ->names('task_statuses');
+
+    Route::resource('tasks', TaskController::class)
+        ->except(['index'])
+        ->names('tasks');
+
+    Route::resource('labels', LabelController::class)
+        ->except(['index', 'show'])
+        ->names('labels');
+});
 
 require __DIR__ . '/auth.php';
